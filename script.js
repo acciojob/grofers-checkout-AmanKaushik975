0b763ab39dfe3f2cc05bb96f7b4a9ee7a7e5a672
AmanKaushik975 <91477500+AmanKaushik975@users.noreply.github.com>
2024-05-01 21:19:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
-
-const getSum = () => {
-//Add your code here
-  
-};
-
-getSumBtn.addEventListener("click", getSum);
-
+const priceArray=document.querySelectorAll('[data-ns-test="price"]');
+let table=document.getElementById("table1");
+let ans=0;
+for (let i = 0; i < priceArray.length; i++) {
+	ans+=parseInt(priceArray[i].innerText);
+}
+let total=document.createElement("tr");
+ 
+total.setAttribute("data-ns-test", "grandTotal");
+ 
+total.innerText=ans;
+ 
+table.appendChild(total);
